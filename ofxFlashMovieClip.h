69d2026e38f356ee676e755cbeff6514bd302152
lukasz.karluk <julapy@googlemail.com>
2011-10-24 23:18:05 +1100

    made ofxFlashMovieClip a friend of ofxFlashXFLBuilder - this way setTotalFrames() and addChildToFrame can remain as private and still be called from ofxFlashXFLBuilder.

--- ofxFlashMovieClip.h
+++ ofxFlashMovieClip.h
@@ -11,6 +11,9 @@
 
 #include "ofxFlashSprite.h"
 
+class ofxFlashXFLBuilder;	// Forward Declarations to prevent Cyclic Dependency.
+                            // http://www.eventhelix.com/RealtimeMantra/HeaderFileIncludePatterns.htm
+
 class ofxFlashMovieClip : public ofxFlashSprite
 {
 
@@ -18,9 +21,8 @@ public:
 	
 	 ofxFlashMovieClip();
 	~ofxFlashMovieClip();
-	
-	void setTotalFrames	( int totalFrames = 1 );
-	ofxFlashDisplayObject* addChildToFrame( ofxFlashDisplayObject* child, int frameNum );
+    
+    friend class ofxFlashXFLBuilder;    // friends! http://www.cplusplus.com/doc/tutorial/inheritance/
 	
 	virtual void setup	() {};
 	virtual void update	() {};
@@ -41,6 +43,9 @@ protected:
 	virtual void updateOnFrame	();
 	
 private:
+    
+	void setTotalFrames	( int totalFrames = 1 );
+	ofxFlashDisplayObject* addChildToFrame( ofxFlashDisplayObject* child, int frameNum );
 	
 	void addFrameChildren		();
 	void removeFrameChildren	();
