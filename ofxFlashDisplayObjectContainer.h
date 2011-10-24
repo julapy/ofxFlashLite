0d8e50ac267249d45e171fa545a52dfc7e696df4
lukasz.karluk <julapy@googlemail.com>
2011-10-24 23:48:35 +1100

    methods for adding / removing / arranging children in ofxFlashDisplayObjectContainer are now virtual so they can be overwritten by ofxFlashMovieClip

--- ofxFlashDisplayObjectContainer.h
+++ ofxFlashDisplayObjectContainer.h
@@ -29,18 +29,18 @@ public:
 	
 	vector<ofxFlashDisplayObject*>	children;
 	
-	ofxFlashDisplayObject*			addChild				( ofxFlashDisplayObject* child );
-	ofxFlashDisplayObject*			addChildAt				( ofxFlashDisplayObject* child, int index );
-	bool							contains				( ofxFlashDisplayObject* child );
-	ofxFlashDisplayObject*			getChildAt				( int index );
-	ofxFlashDisplayObject*			getChildByName			( string name );
-	int								getChildIndex			( ofxFlashDisplayObject* child );
-	vector<ofxFlashDisplayObject*>	getObjectsUnderPoint	( ofPoint point );
-	ofxFlashDisplayObject*			removeChild				( ofxFlashDisplayObject* child );
-	ofxFlashDisplayObject*			removeChildAt			( int index );
-	void							setChildIndex			( ofxFlashDisplayObject* child, int index );
-	void							swapChildren			( ofxFlashDisplayObject* child1, ofxFlashDisplayObject* child2 );
-	void							swapChildrenAt			( int index1, int index2 );
+	virtual ofxFlashDisplayObject*			addChild				( ofxFlashDisplayObject* child );
+	virtual ofxFlashDisplayObject*			addChildAt				( ofxFlashDisplayObject* child, int index );
+	virtual bool							contains				( ofxFlashDisplayObject* child );
+	virtual ofxFlashDisplayObject*			getChildAt				( int index );
+	virtual ofxFlashDisplayObject*			getChildByName			( string name );
+	virtual int								getChildIndex			( ofxFlashDisplayObject* child );
+	virtual vector<ofxFlashDisplayObject*>	getObjectsUnderPoint	( ofPoint point );
+	virtual ofxFlashDisplayObject*			removeChild				( ofxFlashDisplayObject* child );
+	virtual ofxFlashDisplayObject*			removeChildAt			( int index );
+	virtual void							setChildIndex			( ofxFlashDisplayObject* child, int index );
+	virtual void							swapChildren			( ofxFlashDisplayObject* child1, ofxFlashDisplayObject* child2 );
+	virtual void							swapChildrenAt			( int index1, int index2 );
 	
 private:
 	
