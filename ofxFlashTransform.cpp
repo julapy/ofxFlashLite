ae6abb16910ff8485080d5fc7d91d75242a4fa92
lukasz.karluk <julapy@googlemail.com>
2011-12-02 11:33:32 +1100

    added ofxFlashTransform

---
+++ ofxFlashTransform.cpp
@@ -0,0 +1,24 @@
+//
+//  ofxFlashTransform.cpp
+//  emptyExample
+//
+//  Created by lukasz karluk on 2/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "ofxFlashTransform.h"
+
+ofxFlashTransform :: ofxFlashTransform ()
+{
+    //
+}
+
+ofxFlashTransform :: ~ofxFlashTransform ()
+{
+    //
+}
+
+ofxFlashMatrix getRelativeMatrix3D( ofxFlashDisplayObject relativeTo )
+{
+    return ofxFlashMatrix();
+}
---
+++ ofxFlashTransform.h
@@ -0,0 +1,32 @@
+//
+//  ofxFlashTransform.h
+//  emptyExample
+//
+//  Created by lukasz karluk on 2/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "ofMain.h"
+#include "ofxFlashColorTransform.h"
+#include "ofxFlashMatrix.h"
+#include "ofxFlashRectangle.h"
+#include "ofxFlashDisplayObject.h"
+
+class ofxFlashTransform : public ofxFlashObject
+{
+public:
+     ofxFlashTransform ();
+    ~ofxFlashTransform ();
+    
+    /**
+     *  Returns a Matrix3D object, which can transform the space of a specified display object in relation to the current display object's space.
+     **/
+    ofxFlashMatrix getRelativeMatrix3D( ofxFlashDisplayObject relativeTo );
+    
+    ofxFlashColorTransform colorTransform;              // A ColorTransform object containing values that universally adjust the colors in the display object.
+    ofxFlashColorTransform concatenatedColorTransform;  // [read-only] A ColorTransform object representing the combined color transformations applied to the display object and all of its parent objects, back to the root level.
+    ofxFlashMatrix concatenatedMatrix;                  // [read-only] A Matrix object representing the combined transformation matrixes of the display object and all of its parent objects, back to the root level.
+    ofxFlashMatrix matrix;                              // A Matrix object containing values that alter the scaling, rotation, and translation of the display object.
+    ofxFlashMatrix matrix3D;                            // Provides access to the Matrix3D object of a three-dimensional display object.
+    ofxFlashRectangle pixelBounds;                      // [read-only] A Rectangle object that defines the bounding rectangle of the display object on the stage.
+};
