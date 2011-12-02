e844c093b97ddfadae11a1fb83ee768e669cedb9
lukasz.karluk <julapy@googlemail.com>
2011-12-02 14:42:11 +1100

    added update() to ofxFlashTransform

--- src/geom/ofxFlashTransform.cpp
+++ src/geom/ofxFlashTransform.cpp
@@ -18,7 +18,18 @@ ofxFlashTransform :: ~ofxFlashTransform ()
     //
 }
 
-//ofxFlashMatrix getRelativeMatrix3D( ofxFlashDisplayObject relativeTo )
-//{
-//    return ofxFlashMatrix();
-//}
+void ofxFlashTransform :: update ()
+{
+    bDirtyColorTransform = !_colorTransform.isEqual( colorTransform );
+    bDirtyMatrix         = !_matrix.isEqual( matrix );
+    bDirtyMatrix3D       = !_matrix3D.isEqual( matrix3D );
+    
+    if( bDirtyColorTransform )
+        _colorTransform.set( colorTransform );
+    
+    if( bDirtyMatrix )
+        _matrix.set( matrix );
+    
+    if( bDirtyMatrix3D )
+        _matrix3D.set( matrix3D );
+}
--- src/geom/ofxFlashTransform.h
+++ src/geom/ofxFlashTransform.h
@@ -12,16 +12,28 @@
 #include "ofxFlashRectangle.h"
 //#include "ofxFlashDisplayObject.h"
 
+class ofxFlashStage;
+class ofxFlashDisplayObject;
+class ofxFlashBitmap;
+
 class ofxFlashTransform : public ofxFlashObject
 {
 public:
+
      ofxFlashTransform ();
     ~ofxFlashTransform ();
     
     /**
-     *  Returns a Matrix3D object, which can transform the space of a specified display object in relation to the current display object's space.
+     *  friends! http://www.cplusplus.com/doc/tutorial/inheritance/
      **/
-//    ofxFlashMatrix getRelativeMatrix3D( ofxFlashDisplayObject relativeTo );
+    friend class ofxFlashStage;
+    friend class ofxDisplayObject;
+    
+    /**
+     *  Returns a Matrix3D object, which can transform the space of a specified display object in relation to the current display object's space.
+     *  Not working due to cycling dependencies I need to work out.
+     **/
+//    ofxFlashMatrix getRelativeMatrix3D( ofxFlashDisplayObject relativeTo ) { return ofxFlashMatrix(); }
     
     ofxFlashColorTransform colorTransform;              // A ColorTransform object containing values that universally adjust the colors in the display object.
     ofxFlashColorTransform concatenatedColorTransform;  // [read-only] A ColorTransform object representing the combined color transformations applied to the display object and all of its parent objects, back to the root level.
@@ -29,4 +41,24 @@ public:
     ofxFlashMatrix matrix;                              // A Matrix object containing values that alter the scaling, rotation, and translation of the display object.
     ofxFlashMatrix matrix3D;                            // Provides access to the Matrix3D object of a three-dimensional display object.
     ofxFlashRectangle pixelBounds;                      // [read-only] A Rectangle object that defines the bounding rectangle of the display object on the stage.
+    
+protected:
+    
+    void update ();
+    
+private:
+    
+    bool bDirtyColorTransform;
+    bool bDirtyMatrix;
+    bool bDirtyMatrix3D;
+    
+    ofxFlashColorTransform  _colorTransform;
+    ofxFlashColorTransform  _concatenatedColorTransform;
+    ofxFlashMatrix          _concatenatedMatrix;
+    ofxFlashMatrix          _concatenatedMatrixInv;
+    ofxFlashMatrix          _matrix;
+    ofxFlashMatrix          _matrix3D;
+    ofxFlashRectangle       _pixelBounds;
+    ofxFlashRectangle       _rect;
+    ofPoint                 _rectTransformed[ 4 ];
 };
